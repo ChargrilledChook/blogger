70c331b4ddc93a94f0c1ede016ede0832e294292
chook <dylanw212@gmail.com>
2020-07-30 19:13:02 +1000

    tagging feature complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,16 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    Tag.find(params[:id]).destroy
+
+    flash.notice = 'Tag Deleted!'
+
+    redirect_to tags_path(@tags)
+  end
 end
